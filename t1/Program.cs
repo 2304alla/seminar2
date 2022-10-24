6d3499e3ca19c4a2d70c624d94c0fd5b1dd3014b
allakoval <2448572@gmail.com>
2022-10-24 19:39:16 +0300

    t1/t2

--- t1/Program.cs
+++ t1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа. котороае выводит слкчайное число оз отрезка[10,00]; и показывает натбольшую цифру числа
+
+
+
+//int firstDigit = num / 10; // целочнсленное деление на 10, 78/10 = 7.8
+//int secondDigit = num % 10; // остаток от целочисленного деления
+
+//if(firstDigit > secondDigit)  Console.WriteLine($"Наибольшая цифра {firstDigit}");
+//else Console.WriteLine($"Наибольшая цифра {secondDigit}");
+
+//int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+//Console.WriteLine($"Наибольшая цифра числа {num} = {result}");
+
+//int max = Math.Max(firstDigit,secondDigit);
+//Console.WriteLine($"Наибольшая цифра числа {num} = {max}");
+
+int MaxDigit(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    //int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из диапазона 10-99 -> {num}");
+ 
+int maxDigit = MaxDigit(num);
+
+Console.WriteLine($"Наибольшая цифра числа {num} = {maxDigit}");
