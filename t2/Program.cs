14da508123bc7479091caece35ff96c186aa8668
allakoval <2448572@gmail.com>
2022-10-25 13:35:21 +0300

    t3/t4

--- t2/Program.cs
+++ t2/Program.cs
@@ -1,4 +1,25 @@
 ﻿// Программа, которая выводит случайное 3х значное чило и удаляет вторую цифру этого числа
 
+int FirstLastDigit(int number)
+{
+    int number1 = number / 100;
+    int number2 = number % 10;
+    // int result = firstDigit * 10 + thirdDigit;
+    return number1 * 10 + number2;
+}
+
 int num = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное число из диапазона 100-200 -> {num}");
+Console.WriteLine($"Случайное число из диапазона 100-1000 -> {num}");
+
+//int firstDigit = num / 100;
+//int thirdDigit = num % 10;
+//int secondDigit = num /100 % 10;
+
+//Console.Write($"{firstDigit}, {thirdDigit}, {secondDigit}");
+
+
+ int firstLastDigit = FirstLastDigit(num);
+
+Console.WriteLine(FirstLastDigit(num));
+
+
---
+++ t3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимает 2 числа и вявляет, я является ли первое число кротное второму.
+//Если не кртиное 1 число 2, то выводится остаток от деления
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// bool Multiple(int number1, int number2)
+// {
+//     return number1 % number2 == 0;
+// }
+
+// Console.WriteLine(CratNum(num));
+
+int Multiple(int number1, int number2)
+{
+    return number1 % number2;
+}
+
+ int rest = Multiple( num1,  num2);   
+ Console.WriteLine(rest == 0?"Кратно":$"Не кратно, остаток {rest}");
